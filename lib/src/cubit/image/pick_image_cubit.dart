fd1ac9c16b383849c121e4d3c1f15c896e0b8714
NICE\huynh <vuha22.10.20@gmail.com>
2023-02-20 21:28:25 +0700

    delete json loction

--- lib/src/cubit/image/pick_image_cubit.dart
+++ lib/src/cubit/image/pick_image_cubit.dart
@@ -1,11 +1,8 @@
 import 'dart:developer';
-import 'dart:io';
-// import 'dart:io';
 
 import 'dart:typed_data';
 
 import 'package:equatable/equatable.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -17,19 +14,13 @@ class PickImageCubit extends Cubit<PickImageState> {
     emit(PickImageing());
     try {
       final ImagePicker picker = ImagePicker();
-      // final FilePickerResult? image = await FilePicker.platform.pickFiles();
 
       XFile? image = await picker.pickImage(source: ImageSource.gallery);
       if (image != null) {
         var data = await image.readAsBytes();
-        // // File i = File.fromRawPath(file);
-        // inspect(image);
-        // File _image = File(image.path);
-        // inspect(image.readAsString());
-        // inspect(File.fromRawPath(file));
         emit(PickImageSuccess(image: image, data: data));
       } else {
-        // emit(PickImageFailed('Không có hình ảnh được chọn'));
+        emit(const PickImageFailed('Không có hình ảnh được chọn'));
         log('Không có hình ảnh được chọn');
       }
     } catch (e) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,7 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_messaging
-import path_provider_foundation
-import shared_preferences_foundation
 import smart_auth
-import sqflite
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -20,9 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
